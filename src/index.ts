ec64c394dc10dcfc658d7712f885513947b77b06
Guillermo Santos <guillermo.santosba@gmail.com>
2023-07-25 19:48:54 -0600

    Created use case abstract class.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('hello world')
+import * as XMPP from 'stanza';
+const client = XMPP.createClient({
+	jid: 'echobot@example.com',
+	password: 'hunter2',
+
+	// If you have a .well-known/host-meta.json file for your
+	// domain, the connection transport config can be skipped.
+	transports: {
+		websocket: 'wss://example.com:5281/xmpp-websocket',
+		bosh: 'https://example.com:5281/http-bind'
+	}
+});
+
+
+
+client.on('session:started', async () => {
+	
+	client.sendPresence();
+});
+
+client.on('chat', msg => {
+	client.sendMessage({
+		to: msg.from,
+		body: 'You sent: ' + msg.body
+	});
+});
